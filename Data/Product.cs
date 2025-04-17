060d05e29b56ea5cb392e99878da92fe2855fbce
Dan O <droconnell2000@yahoo.com>
2025-04-17 10:17:18 -0400

    End Of Section 13 - Product CRUD

---
+++ Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YumBlazor.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Range(0.01, 1000)]
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string SpecialTag { get; set; } = string.Empty;
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string? ImageUrl { get; set; }
+    
+    }
+}

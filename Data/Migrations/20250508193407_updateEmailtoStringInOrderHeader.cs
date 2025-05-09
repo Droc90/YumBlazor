8ebef35ac47fd27e9bb3de6c761104c1593e3540
Dan O <droconnell2000@yahoo.com>
2025-05-09 07:42:36 -0400

    Section 16 completed added shopping cart

---
+++ Data/Migrations/20250508193407_updateEmailtoStringInOrderHeader.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YumBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEmailtoStringInOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Email",
+                table: "OrderHeader",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
